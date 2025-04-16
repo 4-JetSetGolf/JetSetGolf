d51ea90b14b8af29143865a9abf40abd7a876acf
RobertCummings Jr <robkcjr010210@gmail.com>
2025-04-15 18:54:13 -0700

    css user update

--- src/user-details.jsx
+++ src/user-details.jsx
@@ -1,38 +1,50 @@
 import { useParams, Link } from 'react-router-dom';
 import { useEffect, useState } from 'react';
+import './user-details.css';
 
-const userDetails = () => {
-  const [ userDetails, setUserDetails ] = useState ({});
+function userDetails() {
+  const [userDetails, setUserDetails] = useState({});
 
-  const { userID } = useParams();
+  const { userId } = useParams();
+
+  function App() {
+    return (
+      <Routes>
+        <Route path="/account/:userdetails" element={<userDetails />} />
+      </Routes>
+    );
+  }
 
   useEffect(() => {
-    const getUserDetails = async() => {
+    const getUserDetails = async () => {
       try {
-        const response = await fetch('https://jsonplaceholder.typicode.com/users/${userId}');
+        const response = await fetch(`api/v1/user-details/${userId}`);
         const singleUserDetails = await response.json();
         setUserDetails(singleUserDetails);
-      } catch(err) {
-        console.log(err)
+      } catch (err) {
+        console.log(err);
       }
-    }
-    
+    };
+
     getUserDetails();
   }, []);
 
   return (
-    <section>
-      <h2>Bill Pickleson</h2>
+    <section id="memberpage">
+      <h2 id="member">Bill Pickleson</h2>
+      <li id="points">Rewards</li>
+      <li id="points">Offers</li>
+      <li id="points">Refer a Friend</li>
+     
+
       <p>{userDetails.username}</p>
-
-      {
-        userDetails.address ? <h3>{userDetails.address.city}</h3> : null
-      }
+      
+      {userDetails.address ? <h3>{userDetails.address.city}</h3> : null};
 
 
-      <Link to="/" >BACK</Link>
+      <Link to="/">BACK</Link>
     </section>
-  )
+  );
 }
 
 export default userDetails
